1429d503180168e6c404b3a4c2a730d0ba46af57
jainam singhai <jainamsinghai20460@acropolis.in>
2024-06-22 13:29:11 +0530

    styling changes

--- src/screens/DashboardScreen.js
+++ src/screens/DashboardScreen.js
@@ -9,7 +9,8 @@ import {
   Animated,
   Easing,
   Dimensions,
-  Alert
+  Alert,
+  StyleSheet
 } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import Calendar from '../components/Calendar'; // Assuming Calendar is a custom component
@@ -71,130 +72,82 @@ const AttendanceDashboard = () => {
   };
 
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
+    <GestureHandlerRootView style={styles.root}>
       <TouchableWithoutFeedback onPress={handleOutsidePress}>
         <ScrollView>
-          <View style={{}}>
-            <View style={{ flex: 1, backgroundColor: 'white' }}>
-              <View style={{ flexDirection: 'column', justifyContent: 'space-between', padding: 16, backgroundColor: 'white', borderBottomWidth: 1, borderBottomColor: '#0F0616', fontFamily: 'Satoshi' }}>
-                <Text style={{ fontSize: 24, fontWeight: 'bold', color: '#0F0616', paddingLeft:5,marginTop:-2, fontFamily: 'Satoshi', top: '46px' }}>Mahi Sharma</Text>
-                <Text style={{ fontSize: 24, fontWeight: 'bold', color: '#4E297380', marginTop: 2, paddingLeft: 5 }}>20 May, 2024</Text>
+          <View style={styles.container}>
+            <View style={styles.header}>
+              <View style={styles.headerContent}>
+                <Text style={styles.nameText}>Mahi Sharma</Text>
+                <Text style={styles.dateText}>20 May, 2024</Text>
               </View>
               <TouchableOpacity
-                style={{ position: 'absolute', top: 16, right: 16,marginTop:-3, borderRadius: 4, zIndex: 10 }}
+                style={styles.menuButton}
                 onPress={() => setIsSidebarVisible(!isSidebarVisible)}
               >
-                <Text style={{ color: '#091247', fontSize: 40 }}>≡</Text>
+                <Text style={styles.menuText}>≡</Text>
               </TouchableOpacity>
             </View>
 
             {/* Calendar Component */}
-            <View style={{ paddingTop: 20, paddingLeft: 2, paddingRight: 2, backgroundColor: 'rgba(78, 41, 115, 0.05)', borderRadius: 30, margin: 16, shadowColor: 'black', shadowOpacity: 0.8, shadowRadius: 10 }}>
-              <View style={{ flexDirection: 'row', marginBottom: 10 }}>
-                <Text style={{ fontSize: 24, fontWeight: 'bold', color: '#4E2973', paddingLeft: 10 }}>June</Text>
-                <Text style={{ fontSize: 24, fontWeight: 'bold', color: '#4E297380' }}> 2024</Text>
+            <View style={styles.calendarContainer}>
+              <View style={styles.calendarHeader}>
+                <Text style={styles.monthText}>June</Text>
+                <Text style={styles.yearText}> 2024</Text>
               </View>
               <Calendar />
             </View>
 
             {/* Attendance Summary */}
-            <View style={{
-      flex: 1,
-      flexDirection: 'column',
-      paddingHorizontal: 12,
-      paddingVertical: 24,
-      backgroundColor: 'white',
-      borderRadius: 24,
-      shadowColor: '#000',
-      shadowOffset: { width: 0, height: 1 },
-      shadowOpacity: 0.2,
-      shadowRadius: 1.41,
-      elevation: 2,
-      maxWidth: 500,
-      textAlign: 'center'
-    }}>
-      <View style={{ fontSize: 24 }}>
-        <Text style={{ fontSize: 24, fontWeight: '900', color: '#0F0616' }}>Attendance</Text>
-      </View>
-      <View style={{ fontSize: 16, fontWeight: 'bold', color: '#4B5563', opacity: 0.5 }}>
-        <Text style={{ marginTop: 5, fontSize: 18, fontWeight: 'bold', color: '#4E297380' }}>Out of total working days</Text>
-      </View>
-      <View style={{ marginTop: 14, height: 1, backgroundColor: '#7C3AED', opacity: 0.2 }} />
-      <View style={{
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        paddingHorizontal: 28,
-        paddingVertical: 16,
-        marginTop: 20,
-        borderRadius: 12,
-        backgroundColor: '#2DBEB11A', // teal-400 with opacity 0.1
-      }}>
-        <View style={{ flexDirection: 'column', alignItems: 'flex-start' }}>
-          <View style={{ fontWeight: '900', color: '#1F2937' }}>
-            <Text style={{ fontWeight: '900', color: '#1F2937', fontSize:23 }} >Month</Text>
-          </View>
-          <View style={{  fontWeight: 'bold', color: '#1F2937',  }}>
-            <Text style={{ marginTop: 10, fontWeight: 'bold', color: '#1F2937', opacity: 0.6 }}>May 2024</Text>
-          </View>
-        </View>
-        <View style={{ flexDirection: 'column'}}>
-          <View style={{ letterSpacing: 1 }}>
-            <Text style={{ flexDirection: 'column', fontWeight: '900', color: '#10B981',fontSize:20,marginTop:2}}>18/24</Text>
-          </View>
-          <View style={{ marginTop: 2, alignSelf: 'center' }}>
-            <Text style={{ flexDirection: 'column', fontWeight: '900', color: '#10B981',fontSize:18,marginTop:2}}>Days</Text>
-          </View>
-        </View>
-      </View>
-      <View style={{
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'flex-start',
-        paddingHorizontal: 24,
-        paddingTop: 20,
-        paddingBottom: 12,
-        marginTop: 24,
-        borderRadius: 12,
-        backgroundColor: 'rgba(94, 234, 212, 0.1)', // teal-400 with opacity 0.1
-      }}>
-        <View style={{ flexDirection: 'column' }}>
-          <View style={{ fontWeight: '900', color: '#1F2937' }}>
-            <Text style={{ fontWeight: '900', color: '#1F2937', fontSize:23 }}>Year</Text>
-          </View>
-          <View style={{ marginTop: 5, fontWeight: 'bold', color: '#1F2937' }}>
-            <Text style={{ marginTop: 5, fontWeight: 'bold', color: '#1F2937', opacity: 0.6 }} >2024</Text>
-          </View>
-        </View>
-        <View style={{ flexDirection: 'column', fontWeight: '900', color: '#10B981' }}>
-          <View style={{ letterSpacing: 1 }}>
-            <Text style={{ flexDirection: 'column', fontWeight: '900', color: '#10B981',fontSize:20,marginTop:2}}>179/270</Text>
-          </View>
-          <View style={{ marginTop: 14, alignSelf: 'center' }}>
-            <Text style={{ flexDirection: 'column', fontWeight: '900', color: '#10B981',fontSize:18,marginTop:2}}>Days</Text>
-          </View>
-        </View>
-      </View>
-    </View>
+            <View style={styles.attendanceSummaryContainer}>
+              <View>
+                <Text style={styles.attendanceHeaderText}>Attendance</Text>
+              </View>
+              <View>
+                <Text style={styles.attendanceSubHeaderText}>Out of total working days</Text>
+              </View>
+              <View style={styles.divider} />
+              <View style={styles.summaryBox}>
+                <View style={styles.summaryTextContainer}>
+                  <Text style={styles.summaryTitleText}>Month</Text>
+                  <Text style={styles.summarySubtitleText}>May 2024</Text>
+                </View>
+                <View style={styles.summaryValueContainer}>
+                  <Text style={styles.summaryValueText}>18/24</Text>
+                  <Text style={styles.summaryDaysText}>Days</Text>
+                </View>
+              </View>
+              <View style={styles.summaryBox}>
+                <View style={styles.summaryTextContainer}>
+                  <Text style={styles.summaryTitleText}>Year</Text>
+                  <Text style={styles.summarySubtitleText}>2024</Text>
+                </View>
+                <View style={styles.summaryValueContainer}>
+                  <Text style={styles.summaryValueText}>179/270</Text>
+                  <Text style={styles.summaryDaysText}>Days</Text>
+                </View>
+              </View>
+            </View>
 
             {/* Sidebar */}
             {isSidebarVisible && (
               <TouchableWithoutFeedback onPress={() => setIsSidebarVisible(false)}>
-                <View style={{ position: 'absolute', right: 0, top: 0, bottom: 0, width: 256, backgroundColor: '#f0f0f0', borderLeftWidth: 1, borderLeftColor: 'gray', padding: 20, zIndex: 50 }}>
-                  <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 20 }}>
+                <View style={styles.sidebar}>
+                  <View style={styles.sidebarHeader}>
                     <Image
                       source={{ uri: 'https://via.placeholder.com/50' }} // Replace with the actual profile image URL
-                      style={{ width: 48, height: 48, borderRadius: 24, marginRight: 16 }}
+                      style={styles.profileImage}
                     />
-                    <Text style={{ fontSize: 24, fontWeight: 'bold', color: '#472966' }}>John Doe</Text>
+                    <Text style={styles.profileName}>John Doe</Text>
                   </View>
-                  <TouchableOpacity style={{ paddingVertical: 8 }}>
-                    <Text style={{ fontSize: 18, color: '#714f92' }}>Edit Profile</Text>
+                  <TouchableOpacity style={styles.sidebarItem}>
+                    <Text style={styles.sidebarItemText}>Edit Profile</Text>
                   </TouchableOpacity>
-                  <TouchableOpacity style={{ paddingVertical: 8 }}>
-                    <Text style={{ fontSize: 18, color: '#714f92' }}>Privacy and Security</Text>
+                  <TouchableOpacity style={styles.sidebarItem}>
+                    <Text style={styles.sidebarItemText}>Privacy and Security</Text>
                   </TouchableOpacity>
-                  <TouchableOpacity style={{ paddingVertical: 8 }}>
-                    <Text style={{ fontSize: 18, color: '#714f92' }}>Logout</Text>
+                  <TouchableOpacity style={styles.sidebarItem}>
+                    <Text style={styles.sidebarItemText}>Logout</Text>
                   </TouchableOpacity>
                 </View>
               </TouchableWithoutFeedback>
@@ -202,77 +155,46 @@ const AttendanceDashboard = () => {
           </View>
 
           {/* Slide Popup Component */}
-          <Animated.View style={{
-            transform: [{ translateY: slideAnim }],
-            position: 'absolute',
-            bottom: 0,
-            width: '100%',
-            backgroundColor: 'white',
-            borderTopLeftRadius: 32,
-            borderTopRightRadius: 32,
-            shadowColor: 'black',
-            shadowOpacity: 0.8,
-            shadowRadius: 100,
-            elevation:20,
-            
-            
-          }}>
-            <View style={{ paddingTop: 16, paddingBottom: 64, paddingHorizontal: 16, backgroundColor: 'white', borderTopLeftRadius: 32, borderTopRightRadius: 32, shadowColor: 'black', shadowOpacity: 0.8, shadowRadius: 10, }}>
-            <View style={{display:'flex',justifyContent:'flex-end',width:'100%',alignItems:'flex-end'}} >
-            <TouchableOpacity
-                onPress={handleDoneClick}
-                style={{
-                  
-                  alignItems: 'center',
-                  paddingVertical: 5,
-                  backgroundColor: '#4E2973',
-                  borderRadius: 24,
-                  width:'20%'
-                  
-                }}
-              >
-                
-                <Text style={{ color: 'white', fontWeight: 'bold' }}>Done</Text>
-              </TouchableOpacity>
+          <Animated.View style={[styles.popup, { transform: [{ translateY: slideAnim }] }]}>
+            <View style={styles.popupContent}>
+              <View style={styles.popupButtonContainer}>
+                <TouchableOpacity
+                  onPress={handleDoneClick}
+                  style={styles.doneButton}
+                >
+                  <Text style={styles.doneButtonText}>Done</Text>
+                </TouchableOpacity>
               </View>
-              <View style={{ alignItems: 'center', marginBottom: 5 }}>
-                <Text style={{ fontSize: 20, fontWeight: 'bold', color: '#222B45' }}>Mark Attendance</Text>
+              <View style={styles.popupHeader}>
+                <Text style={styles.popupHeaderText}>Mark Attendance</Text>
+                <Text style={styles.popupDateText}>2 May 2024, Thursday</Text>
               </View>
-              <View style={{ alignItems: 'center', marginBottom: 20 }}>
-                <Text style={{ fontSize: 16, color: '#4E2973', fontWeight: '700' }}>2 May 2024, Thursday</Text>
-              </View>
-              <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginBottom: -30}}>
+              <View style={styles.attendanceOptionsContainer}>
                 <TouchableOpacity
                   onPress={() => handleAttendanceClick('Absent')}
-                  style={{
-                    flex: 1,
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    paddingVertical: 12,
-                    backgroundColor: selectedAttendance === 'Absent' ? '#F84914' : '#fcc3b1',
-                    borderRadius: 24,
-                    marginHorizontal: 8
-                  }}
+                  style={[
+                    styles.attendanceOption,
+                    { backgroundColor: selectedAttendance === 'Absent' ? '#F84914' : '#fcc3b1' },
+                  ]}
                 >
-                  <Text style={{ color: selectedAttendance === 'Absent' ? 'white' : '#F84914', fontWeight: 'bold' }}>Absent</Text>
+                  <Text style={[
+                    styles.attendanceOptionText,
+                    { color: selectedAttendance === 'Absent' ? 'white' : '#F84914' }
+                  ]}>Absent</Text>
                 </TouchableOpacity>
                 <TouchableOpacity
                   onPress={() => handleAttendanceClick('Present')}
-                  style={{
-                    flex: 1,
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    paddingVertical: 12,
-                    backgroundColor: selectedAttendance === 'Present' ? 'rgba(0, 128, 128, 0.8)' : 'rgba(0, 128, 128, 0.2)',
-                    borderRadius: 24,
-                    marginHorizontal: 8
-                  }}
+                  style={[
+                    styles.attendanceOption,
+                    { backgroundColor: selectedAttendance === 'Present' ? 'rgba(0, 128, 128, 0.8)' : 'rgba(0, 128, 128, 0.2)' },
+                  ]}
                 >
-                  <Text style={{ color: selectedAttendance === 'Present' ? 'white' : '#2DBEB1', fontWeight: 'bold' }}>Present</Text>
+                  <Text style={[
+                    styles.attendanceOptionText,
+                    { color: selectedAttendance === 'Present' ? 'white' : '#2DBEB1' }
+                  ]}>Present</Text>
                 </TouchableOpacity>
               </View>
-
-             
             </View>
           </Animated.View>
         </ScrollView>
@@ -280,5 +202,48 @@ const AttendanceDashboard = () => {
     </GestureHandlerRootView>
   );
 };
+
+const styles = StyleSheet.create({
+  root: { flex: 1 },
+  container: {},
+  header: { flex: 1, backgroundColor: 'white' },
+  headerContent: { flexDirection: 'column', justifyContent: 'space-between', padding: 16, backgroundColor: 'white', borderBottomWidth: 1, borderBottomColor: '#0F0616' },
+  nameText: { fontSize: 24, fontWeight: 'bold', color: '#0F0616', paddingLeft: 5, marginTop: -2 },
+  dateText: { fontSize: 24, fontWeight: 'bold', color: '#4E297380', marginTop: 2, paddingLeft: 5 },
+  menuButton: { position: 'absolute', top: 16, right: 16, marginTop: -3, borderRadius: 4, zIndex: 10 },
+  menuText: { color: '#091247', fontSize: 40 },
+  calendarContainer: { paddingTop: 20, paddingLeft: 2, paddingRight: 2, backgroundColor: 'rgba(78, 41, 115, 0.05)', borderRadius: 30, margin: 16, shadowColor: 'black', shadowOpacity: 0.8, shadowRadius: 10 },
+  calendarHeader: { flexDirection: 'row', marginBottom: 10 },
+  monthText: { fontSize: 24, fontWeight: 'bold', color: '#4E2973', paddingLeft: 10 },
+  yearText: { fontSize: 24, fontWeight: 'bold', color: '#4E297380' },
+  attendanceSummaryContainer: { flex: 1, flexDirection: 'column', paddingHorizontal: 12, paddingVertical: 24, backgroundColor: 'white', borderRadius: 24, shadowColor: '#000', shadowOffset: { width: 0, height: 1 }, shadowOpacity: 0.2, shadowRadius: 1.41, elevation: 2, maxWidth: 500, textAlign: 'center' },
+  attendanceHeaderText: { fontSize: 24, fontWeight: '900', color: '#0F0616' },
+  attendanceSubHeaderText: { fontSize: 18, fontWeight: 'bold', color: '#4E297380', marginTop: 5 },
+  divider: { marginTop: 14, height: 1, backgroundColor: '#7C3AED', opacity: 0.2 },
+  summaryBox: { flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: 28, paddingVertical: 16, marginTop: 20, borderRadius: 12, backgroundColor: '#2DBEB11A' },
+  summaryTextContainer: { flexDirection: 'column', alignItems: 'flex-start' },
+  summaryTitleText: { fontWeight: '900', color: '#1F2937', fontSize: 23 },
+  summarySubtitleText: { marginTop: 10, fontWeight: 'bold', color: '#1F2937', opacity: 0.6 },
+  summaryValueContainer: { flexDirection: 'column' },
+  summaryValueText: { fontWeight: '900', color: '#10B981', fontSize: 20, marginTop: 2 },
+  summaryDaysText: { fontWeight: '900', color: '#10B981', fontSize: 18, marginTop: 2 },
+  sidebar: { position: 'absolute', right: 0, top: 0, bottom: 0, width: 256, backgroundColor: '#f0f0f0', borderLeftWidth: 1, borderLeftColor: 'gray', padding: 20, zIndex: 50 },
+  sidebarHeader: { flexDirection: 'row', alignItems: 'center', marginBottom: 20 },
+  profileImage: { width: 48, height: 48, borderRadius: 24, marginRight: 16 },
+  profileName: { fontSize: 24, fontWeight: 'bold', color: '#472966' },
+  sidebarItem: { paddingVertical: 8 },
+  sidebarItemText: { fontSize: 18, color: '#714f92' },
+  popup: { position: 'absolute', bottom: 0, width: '100%', backgroundColor: 'white', borderTopLeftRadius: 32, borderTopRightRadius: 32, shadowColor: 'black', shadowOpacity: 0.8, shadowRadius: 100, elevation: 20 },
+  popupContent: { paddingTop: 16, paddingBottom: 64, paddingHorizontal: 16, backgroundColor: 'white', borderTopLeftRadius: 32, borderTopRightRadius: 32, shadowColor: 'black', shadowOpacity: 0.8, shadowRadius: 10 },
+  popupButtonContainer: { display: 'flex', justifyContent: 'flex-end', width: '100%', alignItems: 'flex-end' },
+  doneButton: { alignItems: 'center', paddingVertical: 5, backgroundColor: '#4E2973', borderRadius: 24, width: '20%' },
+  doneButtonText: { color: 'white', fontWeight: 'bold' },
+  popupHeader: { alignItems: 'center', marginBottom: 5 },
+  popupHeaderText: { fontSize: 20, fontWeight: 'bold', color: '#222B45' },
+  popupDateText: { fontSize: 16, color: '#4E2973', fontWeight: '700', marginBottom: 20 },
+  attendanceOptionsContainer: { flexDirection: 'row', justifyContent: 'space-between', marginBottom: -30 },
+  attendanceOption: { flex: 1, justifyContent: 'center', alignItems: 'center', paddingVertical: 12, borderRadius: 24, marginHorizontal: 8 },
+  attendanceOptionText: { fontWeight: 'bold' },
+});
 
 export default AttendanceDashboard;
