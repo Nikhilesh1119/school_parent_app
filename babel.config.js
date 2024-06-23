7891945d5bd3ecff130d1ac0f76be35cb1cdee04
SubhashKumhar <aks6382@gmail.com>
2024-06-23 11:47:28 +0530

    fonts added

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,17 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: ["nativewind/babel",'react-native-reanimated/plugin'],
+  plugins: ['nativewind/babel','react-native-reanimated/plugin'],
+  presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@src': './src',
+        },
+      },
+    ],
+    'react-native-reanimated/plugin'  // Make sure this is the last plugin
+  ],
 };
